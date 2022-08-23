a8682b54268e13f0471e188bdee66baddded01e1
JaeYoung Bang <80241053+bangjaeyoung@users.noreply.github.com>
2022-08-23 21:34:31 +0900

    Create 12937.java

---
+++ level1/12937.java
@@ -0,0 +1,21 @@
+class Solution {
+   
+  public String solution(int num) {
+       
+      if(num % 2 == 0) {
+           
+        return "Even";
+        }
+        
+    return "Odd";
+    }
+}
+
+
+
+
+// class Solution {
+//     public String solution(int num) {
+//         return num % 2 == 0 ? "Even" : "Odd";
+//     }
+// }
