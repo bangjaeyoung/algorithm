2e5435e723116b0d4e35ffc8a018b28b101e9d26
JaeYoung Bang <80241053+bangjaeyoung@users.noreply.github.com>
2022-08-28 19:18:04 +0900

    Create 12912.java

---
+++ level1/12912.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        
+        if(a > b) {
+            
+            for(int i = 0; i < a - b + 1; i++) {
+                answer += (b + i);
+            }
+            
+        } else if(b > a) {
+            
+            for(int i = 0; i < b - a + 1; i++) {
+                answer += (a + i);
+            }
+            
+        }else {
+            
+            return a;
+        }
+        
+        return answer;
+    }
+}
