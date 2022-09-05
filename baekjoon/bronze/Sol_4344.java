6c1d58485e8938c2047e1644a6997b5e2ee931a8
bangjaeyoung <qkdwo4613@gmail.com>
2022-09-05 22:30:11 +0900

    평균은 넘겠지 - 수정

--- baekjoon/bronze/Sol_4344.java
+++ baekjoon/bronze/Sol_4344.java
@@ -1,34 +1,41 @@
 package baekjoon.bronze;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Sol_4344 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 테스트 케이스의 개수 입력
         // 각 테스트 케이스마다 학생 수 입력
         // 학생 수 만큼 점수 입력
         // 각 테스트 케이스마다 평균
         // 각 테스트 케이스마다 평균보다 높은 점수인 학생들의 비율을 반올림해서 소수점 셋째짜리까지 출력
 
-        Scanner in = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // testCase 개수 입력
+        int testCase = Integer.parseInt(br.readLine());
 
         // 학생 점수 배열
         int[] arr;
 
-        int testCase = in.nextInt();
-
         for (int i = 0; i < testCase; i++) {
 
+            // 학생 수, 성적 입력
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
             // 학생 수 N
-            int N = in.nextInt();
+            int N = Integer.parseInt(st.nextToken());
             arr = new int[N];
 
             // 성적 누적 합 sum
             double sum = 0;
 
-            // 학생 점수들 입력
+            // 학생 점수 배열에 학생 점수 넣기
             for (int j = 0; j < N; j++) {
-                arr[j] = in.nextInt();
+                arr[j] = Integer.parseInt(st.nextToken());
                 sum += arr[j];
             }
 
@@ -47,6 +54,5 @@ public class Sol_4344 {
 
             System.out.println(String.format("%.3f%%", (cnt / arr.length) * 100));
         }
-        in.close();
     }
 }
