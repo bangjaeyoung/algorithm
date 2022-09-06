621894c4892b740eefd5831fcb2195f33afabf5d
bangjaeyoung <qkdwo4613@gmail.com>
2022-09-06 22:34:03 +0900

    정수 N개의 합

---
+++ baekjoon/bronze/Sol_15596.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze;
+
+public class Sol_15596 {
+
+    long sum(int[] a) {
+
+        int sum = 0;
+
+        for (int n : a) {
+            sum += n;
+        }
+
+        return sum;
+    }
+}
