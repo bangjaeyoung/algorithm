a810c78a6148c75a94faa003a2e7f5f23bd1f732
bangjaeyoung <qkdwo4613@gmail.com>
2022-09-22 22:26:43 +0900

    자연수 뒤집어 배열로 만들기

--- programmers/level1/Sol_12932.java
+++ programmers/level1/Sol_12932.java
@@ -1,18 +1,9 @@
 package programmers.level1;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class Sol_12932 {
 
-    public static void main(String[] args) {
-
-        Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.solution(1234567)));
-    }
-}
-
-class Solution {
     public int[] solution(long n) {
 
         String str = String.valueOf(n);
