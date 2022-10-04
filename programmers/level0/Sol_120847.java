a17c909798f1d2b70c4fd61c00af66407c7699be
bangjaeyoung <qkdwo4613@gmail.com>
2022-10-04 15:36:38 +0900

    최대값 만들기(1)

---
+++ programmers/level0/Sol_120847.java
@@ -0,0 +1,14 @@
+package programmers.level0;
+
+import java.util.Arrays;
+
+public class Sol_120847 {
+    public int solution(int[] numbers) {
+
+        // 배열 numbers를 오름차순으로 정렬
+        Arrays.sort(numbers);
+
+        // 배열의 마지막 요소와 (마지막 - 1)번째 요소를 곱하면 최댓값
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
+    }
+}
