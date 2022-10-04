e8ed08259ddd20a3b5b80428b910714f4fd11676
bangjaeyoung <qkdwo4613@gmail.com>
2022-10-04 15:46:31 +0900

    옷가게 할인 받기

---
+++ programmers/level0/Sol_120818.java
@@ -0,0 +1,21 @@
+package programmers.level0;
+
+public class Sol_120818 {
+    public int solution(int price) {
+
+        double discountRate = 0;
+
+        if (price >= 100_000) {
+            discountRate = 0.05;
+
+            if (price >= 300_000) {
+                discountRate = 0.1;
+
+                if (price >= 500_000) {
+                    discountRate = 0.2;
+                }
+            }
+        }
+        return (int) Math.floor(price * (1 - discountRate));
+    }
+}
