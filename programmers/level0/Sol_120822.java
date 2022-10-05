62df46c379ba1ce3dc73020607e1ead21d10c272
bangjaeyoung <qkdwo4613@gmail.com>
2022-10-05 22:30:42 +0900

    문자열 뒤집기

---
+++ programmers/level0/Sol_120822.java
@@ -0,0 +1,10 @@
+package programmers.level0;
+
+public class Sol_120822 {
+    public String solution(String myString) {
+
+        StringBuilder sb = new StringBuilder(myString);
+
+        return sb.reverse().toString();
+    }
+}
