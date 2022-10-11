e0d620c1dc43bf94e87033e42c80ef7ca7ed5b8d
bangjaeyoung <qkdwo4613@gmail.com>
2022-10-11 22:46:28 +0900

    피자 나눠 먹기 (2)

---
+++ programmers/level0/Sol_120815.java
@@ -0,0 +1,23 @@
+package programmers.level0;
+
+public class Sol_120815 {
+    public int solution(int n) {
+
+        int d = gcd(n, 6);
+
+        // 최소공배수 / 6(피자 6조각)
+        return (n * 6 / d) / 6;
+    }
+
+    // 최대공약수 구하는 메서드
+    public int gcd(int a, int b) {
+
+        while (b != 0) {
+            int r = a % b;
+
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
