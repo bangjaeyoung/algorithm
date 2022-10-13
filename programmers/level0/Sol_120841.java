8a7d4360ff2417cc53fa7f9781afb618fedaebe8
bangjaeyoung <qkdwo4613@gmail.com>
2022-10-13 22:58:53 +0900

    점의 위치 구하기

---
+++ programmers/level0/Sol_120841.java
@@ -0,0 +1,18 @@
+package programmers.level0;
+
+public class Sol_120841 {
+    public int solution(int[] dot) {
+
+        // 제 1사분면
+        if (dot[0] > 0 && dot[1] > 0) return 1;
+
+            // 제 2사분면
+        else if (dot[0] < 0 && dot[1] > 0) return 2;
+
+            // 제 3사분면
+        else if (dot[0] < 0 && dot[1] < 0) return 3;
+
+            // 제 4사분면
+        else return 4;
+    }
+}
