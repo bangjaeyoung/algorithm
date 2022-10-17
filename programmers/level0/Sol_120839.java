165ab1d1b7a868e7a7f2c3b8fa6c90f8c45b131a
bangjaeyoung <qkdwo4613@gmail.com>
2022-10-17 22:11:56 +0900

    가위 바위 보

---
+++ programmers/level0/Sol_120839.java
@@ -0,0 +1,28 @@
+package programmers.level0;
+
+public class Sol_120839 {
+    public String solution(String rsp) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < rsp.length(); i++) {
+
+            switch (rsp.charAt(i)) {
+
+                case '2':
+                    sb.append("0");
+                    break;
+
+                case '0':
+                    sb.append("5");
+                    break;
+
+                default:
+                    sb.append("2");
+                    break;
+            }
+        }
+
+        return String.valueOf(sb);
+    }
+}
