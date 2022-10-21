a6fb5d45ae99b5edc50b0b9f1bcdddc4eaa072e6
bangjaeyoung <qkdwo4613@gmail.com>
2022-10-21 22:49:22 +0900

    update 2022. 10. 21.

--- programmers/level1/Sol_1845.java
+++ programmers/level1/Sol_1845.java
@@ -8,10 +8,12 @@ public class Sol_1845 {
 
         Map<Integer, Integer> numsHashMap = new HashMap<>();
 
+        // 중복된 폰켓몬 종류는 하나의 종류로 취급
         for (int num : nums) {
             numsHashMap.put(num, 1);
         }
 
-        return numsHashMap.size();
+        // 최대로 나올 수 있는 종류는 N / 2 마리
+        return Math.min(numsHashMap.size(), nums.length / 2);
     }
 }
