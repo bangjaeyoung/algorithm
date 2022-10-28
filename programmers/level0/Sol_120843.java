f2497b7318d3610f36f1095095931f4c98401d4b
bangjaeyoung <qkdwo4613@gmail.com>
2022-10-28 22:13:17 +0900

    2022. 10. 28.

---
+++ programmers/level0/Sol_120843.java
@@ -0,0 +1,19 @@
+package programmers.level0;
+
+public class Sol_120843 {
+    public int solution(int[] numbers, int k) {
+
+        int answer = 0;
+        int kCount = 0;
+
+        for (int i = 0; i < numbers.length; ) {
+            answer = numbers[i];
+            kCount++;
+            if (kCount == k) break;
+            i += 2;
+            if (i == numbers.length) i = 0;
+            if (i == numbers.length + 1) i = 1;
+        }
+        return answer;
+    }
+}
