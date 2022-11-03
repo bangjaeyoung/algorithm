f6f946e36d7d80426dab337a90437cbb3c3a4b30
bangjaeyoung <qkdwo4613@gmail.com>
2022-11-03 20:52:57 +0900

    소인수분해

---
+++ programmers/level0/Sol_120852.java
@@ -0,0 +1,20 @@
+package programmers.level0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sol_120852 {
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                answer.add(i);
+                n /= i;
+            }
+        }
+        if (n != 1) answer.add(n);
+
+        return answer.stream().distinct().mapToInt(i -> i).toArray();
+    }
+}
