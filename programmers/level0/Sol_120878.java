1d9362d98033ce4a6f25f2bdfb72e256fd875fc1
bangjaeyoung <qkdwo4613@gmail.com>
2022-11-29 16:37:54 +0900

    Update 유한소수 판별하기

--- programmers/level0/Sol_120878.java
+++ programmers/level0/Sol_120878.java
@@ -1,37 +1,20 @@
 package programmers.level0;
 
-import java.util.Stack;
-
 // 유한소수이면 1, 무한소수이면 2 반환
 class Sol_120878 {
     public int solution(int a, int b) {
-        int result = 2;
+        int dividedB = b / gcd(a, b);
 
-        // 정수는 유한소수로 분류
-        if (a % b == 0) return 1;
-
-        a /= gcd(a, b);
-        b /= gcd(a, b);
-
-        // 소인수가 2와 5뿐인지 판별하는 과정
-//        Stack<Integer> divisor = new Stack<>();
-//
-//        for (int i = 2; i <= b; i++) {
-//            if (b % i == 0) {
-//                divisor.add(i);
-//                while (b % i == 0) {
-//                    b /= i;
-//                }
-//            }
-//        }
-//
-//        if (divisor.peek() == 2 || divisor.peek() == 5) {
-//            divisor.pop();
-//            result = 1;
-//            if (divisor.peek() == 2) result = 1;
-//        }
-
-        return result;
+        while (dividedB != 1) {
+            if (dividedB % 2 == 0) {
+                dividedB /= 2;
+            } else if (dividedB % 5 == 0) {
+                dividedB /= 5;
+            } else {
+                return 2;
+            }
+        }
+        return 1;
     }
 
     // 최대공약수
