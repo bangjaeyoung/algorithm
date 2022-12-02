a85b09ee8fab6c3c4e17c10a8c5044b176bb33dd
bangjaeyoung <qkdwo4613@gmail.com>
2022-12-02 22:05:52 +0900

    다항식 더하기

---
+++ programmers/level0/Sol_120863.java
@@ -0,0 +1,34 @@
+package programmers.level0;
+
+public class Sol_120863 {
+    public String solution(String polynomial) {
+        String[] arr = polynomial.split(" ");
+        int coefficient = 0;
+        int constant = 0;
+
+        for (String ar : arr) {
+            if (ar.equals("+")) continue;
+
+            if (ar.contains("x")) {
+                String temp = ar.replace("x", "");
+                // x 계수가 1이면
+                if (temp.equals("")) coefficient += 1;
+                else coefficient += Integer.parseInt(temp);
+            }
+            // 상수이면
+            else constant += Integer.parseInt(ar);
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        if (coefficient > 1) {
+            result.append(coefficient).append("x");
+            if (constant != 0) result.append(" + ").append(constant);
+        } else if (coefficient == 1) {
+            result.append("x");
+            if (constant != 0) result.append(" + ").append(constant);
+        } else if (constant != 0) result.append(constant);
+
+        return String.valueOf(result);
+    }
+}
