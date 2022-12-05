7d82f60a33ef5f5486702295773b03979c904b92
bangjaeyoung <qkdwo4613@gmail.com>
2022-12-05 20:58:06 +0900

    평행

---
+++ programmers/level0/Sol_120875.java
@@ -0,0 +1,19 @@
+package programmers.level0;
+
+public class Sol_120875 {
+    public int solution(int[][] dots) {
+        double slopA = (double) (dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0]);
+        double slopB = (double) (dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]);
+        if (slopA == slopB) return 1;
+
+        slopA = (double) (dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0]);
+        slopB = (double) (dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]);
+        if (slopA == slopB) return 1;
+
+        slopA = (double) (dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]);
+        slopB = (double) (dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]);
+        if (slopA == slopB) return 1;
+
+        return 0;
+    }
+}
