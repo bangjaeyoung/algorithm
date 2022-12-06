89cd7d13c8cddf0898a6dee2c97c9c4d2c43c484
bangjaeyoung <qkdwo4613@gmail.com>
2022-12-06 21:07:49 +0900

    Update 겹치는 선분의 길이

--- programmers/level0/Sol_120876.java
+++ programmers/level0/Sol_120876.java
@@ -1,48 +1,23 @@
 package programmers.level0;
 
 public class Sol_120876 {
-    public static int solution(int lines[][]) {
-        int minNum = 100;
-        int maxNum = -100;
-        for (int i = 0; i < lines.length; i++) {
-            for (int j = 0; j < lines[i].length; j++) {
-                if (lines[i][j] < minNum) {
-                    minNum = lines[i][j];
-                }
-                if (lines[i][j] > maxNum) {
-                    maxNum = lines[i][j];
-                }
-            }
-        }
-        if (minNum < 0) {
-            for (int i = 0; i < lines.length; i++) {
-                for (int j = 0; j < lines[i].length; j++) {
-                    lines[i][j] += Math.abs(minNum);
-                }
+    public int solution(int[][] lines) {
+        int answer = 0;
+        int[] num = new int[200];
+
+        for (int[] line : lines) {
+            int minNum = Math.min(line[0], line[1]);
+            int maxNum = Math.max(line[0], line[1]);
+            for (int i = minNum; i < maxNum; i++) {
+                num[i + 100]++;
             }
         }
 
-        int[] numCheck = new int[maxNum - minNum + 2];
-
-        for (int i = 0; i < lines.length; i++) {
-            for (int j = lines[i][0]; j <= lines[i][1]; j++) {
-                numCheck[j]++;
+        for (int n : num) {
+            if (n > 1) {
+                answer++;
             }
         }
-
-        int lengthOfOverlappingLine = 0;
-        for (int i = 0; i < numCheck.length - 1; i++) {
-            if (numCheck[i] > 1) {
-                if (numCheck[i + 1] > 1) {
-                    lengthOfOverlappingLine++;
-                }
-            }
-        }
-        return lengthOfOverlappingLine;
-    }
-
-    public static void main(String[] args) {
-        int[][] ex = {{-1, 0}, {0, 1}, {1, 2}};
-        System.out.println(solution(ex));
+        return answer;
     }
 }
