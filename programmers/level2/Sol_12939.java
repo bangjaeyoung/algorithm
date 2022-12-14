a04985c1cda0fa52079f58fb16bcd722e0881230
bangjaeyoung <qkdwo4613@gmail.com>
2022-12-15 00:16:55 +0900

    Update 최댓값과 최솟값

--- programmers/level2/Sol_12939.java
+++ programmers/level2/Sol_12939.java
@@ -4,21 +4,7 @@ import java.util.Arrays;
 
 public class Sol_12939 {
     public String solution(String s) {
-        String[] strArr = s.split(" ");
-        Arrays.sort(strArr);
-
-        int minusCheck = 0;
-        for (String str : strArr) {
-            if (str.charAt(0) == '-') minusCheck++;
-        }
-
-        StringBuilder answer = new StringBuilder();
-        if (minusCheck == strArr.length) {  // 모든 수가 음수일 경우
-            answer.append(strArr[strArr.length - 1]).append(" ").append(strArr[0]);
-        } else {
-            answer.append(strArr[0]).append(" ").append(strArr[strArr.length - 1]);
-        }
-
-        return String.valueOf(answer);
+        Object[] intArr = Arrays.stream(s.split(" ")).map(Integer::parseInt).sorted().toArray();
+        return intArr[0] + " " + intArr[intArr.length - 1];
     }
 }
