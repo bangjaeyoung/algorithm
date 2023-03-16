46bde220c9f06a856692dd2efe768ea165974222
bangjaeyoung <qkdwo4613@gmail.com>
2023-03-16 22:05:46 +0900

    평균

--- baekjoon/bronze/Sol_1546.java
+++ baekjoon/bronze/Sol_1546.java
@@ -1,33 +1,26 @@
 package baekjoon.bronze;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sol_1546 {
     public static void main(String[] args) {
 
-        // 입력 받은 수만큼의 크기를 가진 배열을 만든다.
-        // 배열을 돌면서, 배열의 요소를 하나씩 입력 받는다.
-        // 배열의 요소들을 오름차순으로 정렬한다.
-        // 배열을 돌면서, sum(합계)에 공식을 적용한 값을 넣는다.
-        // 평균을 출력한다.
-
         Scanner sc = new Scanner(System.in);
 
-        double[] gradeArr = new double[sc.nextInt()];
+        int N = sc.nextInt();
 
-        for(int i = 0; i < gradeArr.length; i++) {
-            gradeArr[i] = sc.nextDouble();
-        }
-        sc.close();
-
-        Arrays.sort(gradeArr);
-        double sum = 0;
-
-        for (double arr : gradeArr) {
-            sum += ((arr / gradeArr[gradeArr.length - 1]) * 100);
+        int[] scores = new int[N];
+        for (int i = 0; i < N; i++) {
+            scores[i] = sc.nextInt();
         }
 
-        System.out.println(sum / gradeArr.length);
+        int M = 0;
+        int sum = 0;
+        for (int score : scores) {
+            M = Math.max(M, score);
+            sum += score;
+        }
+
+        System.out.print((sum * 100.0 / M) / N);
     }
 }
