cdc658ce6d7d50e896d4a08678abe7b1003f6bf7
bangjaeyoung <qkdwo4613@gmail.com>
2023-03-16 21:11:10 +0900

    숫자의 합

--- baekjoon/bronze/Sol_11720.java
+++ baekjoon/bronze/Sol_11720.java
@@ -3,21 +3,20 @@ package baekjoon.bronze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Sol_11720 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        br.readLine();
+        int N = Integer.parseInt(br.readLine());
+
+        char[] charNum = br.readLine().toCharArray();
 
         int sum = 0;
+        for (char ch : charNum)
+            sum += ch - '0';
 
-        for (byte a : br.readLine().getBytes()) {
-            sum += (a - '0');
-        }
-
-        System.out.println(sum);
+        System.out.print(sum);
     }
 }
