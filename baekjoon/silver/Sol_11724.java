e05ec2ad408221a4e37845cc2d7e5d1435b44028
bangjaeyoung <qkdwo4613@gmail.com>
2023-03-21 20:49:57 +0900

    Solve 연결 요소의 개수

---
+++ baekjoon/silver/Sol_11724.java
@@ -0,0 +1,60 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// DFS
+public class Sol_11724 {
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            graph.get(from).add(to);
+            graph.get(to).add(from);
+        }
+
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+
+    private static void dfs(int node) {
+        if (visited[node]) return;
+
+        visited[node] = true;
+        int size = graph.get(node).size();
+        for (int i = 0; i < size; i++) {
+            int value = graph.get(node).get(i);
+            if (!visited[value]) {
+                dfs(value);
+            }
+        }
+    }
+}
