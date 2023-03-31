2baafbc729a4c6de557302406e4b66c5571ea05c
Jaeyoung Bang <qkdwo4613@gmail.com>
2023-03-31 23:10:07 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -2,74 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static int m;
-    static char[][] board;
-    static char[][] copyBoard;
+    static boolean[][] board;
+    static int answer = 64;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        board = new char[n][m];
-        copyBoard = new char[n][m];
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        board = new boolean[n][m];
 
-
-        // 보드 세팅
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
                 char ch = line.charAt(j);
-                board[i][j] = ch;
-                copyBoard[i][j] = ch;
+                if (ch == 'W') board[i][j] = true;
             }
         }
 
-        int answer = 64;
-
         for (int i = 0; i < n - 7; i++) {
             for (int j = 0; j < m - 7; j++) {
-                int count = count(copyBoard, i, j);
-                answer = Math.min(answer, count);
-                initializeBoard();
+                count(i, j);
             }
         }
 
         System.out.println(answer);
     }
 
-    private static int count(char[][] board, int startRow, int startCol) {
+    private static void count(int startRow, int startCol) {
+        boolean color = board[startRow][startCol];
         int count = 0;
-
-        for (int i = startRow; i < startRow + 7; i++) {
-            for (int j = startCol; j < startCol + 7; j++) {
-                char now = board[i][j];
-
-                if (now == board[i][j + 1]) {
+        
+        for (int i = startRow; i < startRow + 8; i++) {
+            for (int j = startCol; j < startCol + 8; j++) {
+                if (board[i][j] != color) {
                     count++;
-                    if (now == 'B') board[i][j + 1] = 'W';
-                    else board[i][j + 1] = 'B';
-                }
-
-                if (now == board[i + 1][j]) {
-                    count++;
-                    if (now == 'B') board[i + 1][j] = 'W';
-                    else board[i + 1][j] = 'B';
                 }
                 
-                if (i == startRow + 6 && j == startCol + 6) {
-                    if (now != board[i + 1][j + 1]) count++;
-                }
+                color = !color;
             }
+            
+            color = !color;
         }
 
-        return Math.min(count, 64 - count);
-    }
-
-    private static void initializeBoard() {
-        for (int i = 0; i < copyBoard.length; i++) {
-            copyBoard[i] = Arrays.copyOf(board[i], board[i].length);
-        }
+        count = Math.min(count, 64 - count);
+        
+        answer = Math.min(answer, count);
     }
 }
