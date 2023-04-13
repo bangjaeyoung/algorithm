37afe658180108a042b8b37e805fa9958c4886d5
Jaeyoung Bang <qkdwo4613@gmail.com>
2023-04-13 17:40:15 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        return 45 - sum;
+    }
+}
