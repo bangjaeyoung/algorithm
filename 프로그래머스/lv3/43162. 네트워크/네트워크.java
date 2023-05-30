2233d6dd3d53d8b2189219619a0e5741c4251289
Jaeyoung Bang <qkdwo4613@gmail.com>
2023-05-30 15:52:16 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -5,24 +5,21 @@ class Solution {
     static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
-        // 노드들의 방문 여부를 체크할 배열 생성
         visited = new boolean[n];
-        // 노드들에 연결된 노드를 추가할 배열 리스트를 추가
+        
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
-        // 노드들에 연결된 노드를 추가
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers.length; j++) {
-                int from = i;
-                int to = j;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (computers[i][j] == 1) {
-                    graph.get(from).add(to);
-                    graph.get(to).add(from);
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
                 }
             }
         }
-        // 방문하지 않은 노드를 대상으로 dfs 진행, count 1 증가
+        
         int count = 0;
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
@@ -30,14 +27,14 @@ class Solution {
                 count++;
             }
         }
+        
         return count;
     }
     
     private void dfs(int node) {
         if (visited[node]) return;
-        // 해당 노드의 방문한 걸로 바꾸기
+        
         visited[node] = true;
-        // 해당 노드에 연결된 모든 노드들을 순회하는데, 방문하지 않은 노드만 dfs 진행
         int size = graph.get(node).size();
         for (int i = 0; i < size; i++) {
             int value = graph.get(node).get(i);
