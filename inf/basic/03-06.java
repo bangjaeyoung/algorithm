2db28bdd0480145f30b69f3f76cb6b391782547d
Jaeyoung Bang <qkdwo4613@gmail.com>
2023-06-29 15:45:23 +0900

    Create 03-06.java
    two pointers

---
+++ inf/basic/03-06.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public int solution(int n, int k, int[] arr) {
+        int ans = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > k) {
+                if (arr[lt++] == 0) cnt--;
+            }
+            ans = Math.max(ans, rt-lt+1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.print(main.solution(n, k, arr));
+    }
+}
