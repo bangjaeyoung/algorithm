59b2eaecf1867b0bd9c200cfe85d72a12fea3558
Jaeyoung Bang <qkdwo4613@gmail.com>
2023-07-19 22:20:57 +0900

    Create 06-03.java
    - algorithm: 삽입정렬
    - description:
    i는 1부터 n까지 증가하면서 순회
    j는 i-1부터 0과 같을 때까지 작아지면서 순회
    i 인덱스의 값을 tmp로 두고, j 인덱스의 값보다 작으면 서로 위치 변경
    j를 i for문에서 선언을 하고, j for문을 빠져나온 후에 j+1 인덱스에 tmp 값을 두면서 마무리 위치 변화 작업을 진행

---
+++ inf/basic/06-03.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+  
+public class Main {
+  private int[] solution(int n, int[] arr) {
+    for (int i=1; i<n; i++) {
+      int tmp = arr[i], j;
+      for (j=i-1; j>=0; j--) {
+        if (arr[j] > tmp)
+          arr[j+1] = arr[j];
+        else break;
+      }
+      arr[j+1] = tmp;
+    }
+    return arr;
+  }
+  
+  public static void main(String[] args) {
+    Main main = new Main();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i=0; i<n; i++)
+      arr[i] = sc.nextInt();
+    for (int a : main.solution(n, arr))
+      System.out.print(a + " ");
+  }
+}
