e7c3095146f7b1e9ae468e723c82d6e0fe0868ba
Jaeyoung Bang <qkdwo4613@gmail.com>
2023-07-30 21:16:53 +0900

    Create 07-14.java
    - algorithm : 그래프 최단거리(BFS)
    - description : 
    1. 중첩 ArrayList를 통해 그래프를 표현하고 연결
    2. 거리, 레벨 배열 생성
    3. BFS - Queue, While문 사용

---
+++ inf/basic/07-14.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+  static List<List<Integer>> graph;
+  static int n, m;
+  static int[] distance, check;
+
+  public static void main(String[] args) {
+    Main main = new Main();
+    Scanner sc = new Scanner(System.in);
+
+    n = sc.nextInt();
+    m = sc.nextInt();
+    graph = new ArrayList<>();
+    distance = new int[n + 1];
+    check = new int[n + 1];
+
+    for (int i = 0; i <= n; i++) {
+      graph.add(new ArrayList<>());
+    }
+
+    for (int i = 0; i < m; i++) {
+      int from = sc.nextInt(), to = sc.nextInt();
+      graph.get(from).add(to);
+    }
+
+    main.bfs(1);
+
+    for (int i = 2; i <= n; i++) {
+      System.out.println(i + " : " + distance[i]);
+    }
+  }
+
+  private void bfs(int vertex) {
+    Queue<Integer> queue = new LinkedList<>();
+    queue.offer(vertex);
+    check[vertex] = 1;
+    int level = 0;
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+
+      for (int i = 0; i < size; i++) {
+        int currentVertex = queue.poll();
+        distance[currentVertex] = level;
+
+        for (int nextVertex : graph.get(currentVertex)) {
+          if (check[nextVertex] == 0) {
+            check[nextVertex] = 1;
+            queue.offer(nextVertex);
+          }
+        }
+      }
+
+      level++;
+    }
+  }
+}
