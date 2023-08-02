529af0d389750fb3d971cd171064954b0406491f
Jaeyoung Bang <qkdwo4613@gmail.com>
2023-08-02 23:25:56 +0900

    Create 10-03.java
    - algorithm : 최장 증가 부분 수열(LIS)
    - description : 
    dp 이용
    문제 풀이에 대한 원리를 잘 파악하고 시작하자

---
+++ inf/basic/10-03.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static int[] arr, dy;
+  static int len;
+  
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    
+    arr = new int[n];
+    len = arr.length;
+    dy = new int[n];
+    
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    
+    System.out.print(solution());
+  }
+  
+  private static int solution() {
+    int answer = 0;
+    dy[0] = 1;
+    
+    for (int i = 1; i < len; i++) {
+      int max = 0;
+      
+      for (int j = 0; j <= i-1; j++) {
+        if (arr[i] > arr[j] && dy[j] > max) {
+          max = dy[j];
+        }
+        
+        dy[i] = max + 1;
+        answer = Math.max(answer, dy[i]);
+      }
+    }
+    
+    return answer;
+  }
+}
