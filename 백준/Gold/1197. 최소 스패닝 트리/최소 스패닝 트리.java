5bb2381c32d0aaa0ca7cdd2e6d61f88dc66fd80a
Jaeyoung Bang <qkdwo4613@gmail.com>
2023-08-30 13:30:21 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 672 ms, Memory: 49736 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 /**
- * Union&Find Algorithm
+ * Union & Find Algorithm
  */
 class Edge implements Comparable<Edge> {
     int v1;
@@ -24,7 +24,7 @@ class Edge implements Comparable<Edge> {
 public class Main {
     static int[] unf;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -34,13 +34,11 @@ public class Main {
         unf = new int[V + 1];
         List<Edge> edges = new ArrayList<>();
 
-        for (int i = 1; i <= V; i++) {
+        for (int i = 1; i <= V; i++)
             unf[i] = i;
-        }
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-
             int v1 = Integer.parseInt(st.nextToken());
             int v2 = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
@@ -53,9 +51,9 @@ public class Main {
         int ans = 0;
         int cnt = 0;
 
-        for (Edge e : edges) {
-            if (union(e.v1, e.v2)) {
-                ans += e.cost;
+        for (Edge edge : edges) {
+            if (union(edge.v1, edge.v2)) {
+                ans += edge.cost;
                 cnt++;
 
                 if (cnt == E - 1) break;
@@ -65,14 +63,14 @@ public class Main {
         System.out.println(ans);
     }
 
-    private static int find(int v) {
+    static int find(int v) {
         if (unf[v] == v)
             return v;
         else
             return unf[v] = find(unf[v]);
     }
 
-    private static boolean union(int v1, int v2) {
+    static boolean union(int v1, int v2) {
         int fv1 = find(v1);
         int fv2 = find(v2);
 
