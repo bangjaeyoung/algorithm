b342c849d69c9ff9f515702c67e6709732832c33
Jaeyoung <qkdwo4613@gmail.com>
2023-12-11 11:24:54 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 732 ms, Memory: 52140 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -24,31 +24,22 @@ public class Main {
         dis = new int[n][m];
         visited = new boolean[n][m];
         
+        int startX = 0;
+        int startY = 0;
         for (int h = 0; h < n; h++) {
             st = new StringTokenizer(br.readLine());
             for (int w = 0; w < m; w++) {
                 board[h][w] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        Loop:
-        for (int h = 0; h < n; h++) {
-            for (int w = 0; w < m; w++) {
-                if (board[h][w] == 2) {
-                    bfs(h, w);
-                    break Loop;
-                }
-            }
-        }
-        
-        // 원래 갈 수 있는 땅인 부분 중에서 도달할 수 없는 위치 설정 -> -1
-        for (int h = 0; h < n; h++) {
-            for (int w = 0; w < m; w++) {
-                if (board[h][w] == 1 && dis[h][w] == 0) {
+                if (board[h][w] == 1) {
                     dis[h][w] = -1;
+                } else if (board[h][w] == 2) {
+                    startX = h;
+                    startY = w;
                 }
             }
         }
+        
+        bfs(startX, startY);
         
         for (int h = 0; h < n; h++) {
             for (int w = 0; w < m; w++) {
@@ -73,7 +64,7 @@ public class Main {
                 int nextY = cur[1] + dy[i];
                 
                 if (nextX >= 0 && nextY >= 0 && nextX < n && nextY < m) {
-                    if (!visited[nextX][nextY] && board[nextX][nextY] == 1) {
+                    if (!visited[nextX][nextY] && board[nextX][nextY] != 0) {
                         dis[nextX][nextY] = dis[cur[0]][cur[1]] + 1;
                         queue.offer(new int[]{nextX, nextY});
                         visited[nextX][nextY] = true;
