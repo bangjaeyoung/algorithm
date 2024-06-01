0d5327a0f03e2597bf70ed695682c4747f2210a5
Jaeyoung <qkdwo4613@gmail.com>
2024-06-01 18:46:17 +0900

    [level 0] Title: n 번째 원소부터, Time: 5.61 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.kt
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.kt
@@ -1,5 +1,9 @@
 class Solution {
     fun solution(numList: IntArray, n: Int): IntArray {
-        return numList.sliceArray(n - 1 until numList.size)
+        val answer = mutableListOf<Int>()
+        for (idx in n - 1 until numList.size) {
+            answer.add(numList[idx])
+        }
+        return answer.toIntArray()
     }
 }
