94359e5d33f6d864780be4bf67db5030336d230d
Jaeyoung <qkdwo4613@gmail.com>
2024-06-03 18:01:22 +0900

    [level 0] Title: 카운트 다운, Time: 8.36 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.kt
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.kt
@@ -1,9 +1,5 @@
 class Solution {
     fun solution(startNum: Int, endNum: Int): List<Int> {
-        val answer = mutableListOf<Int>()
-        for (i in startNum downTo endNum) {
-            answer.add(i)
-        }
-        return answer
+        return (startNum downTo endNum).toList()
     }
 }
